88c7a7faae073b955ad54c0a4047721bb5701064
Cody <ccoff1798@gmail.com>
2023-08-05 17:53:40 -0600

    got the base files added and generated API Key

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById('user-input')
+const search = document.getElementById('search')
+
+let lat = 0
+let lon = 0
+let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid=853c7e45dec1706d5f746a46f8d87cf8'
+
