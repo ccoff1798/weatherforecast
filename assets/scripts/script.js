6882d3bb0c66e33e2f5de2dfd08e63691ec6e32a
Cody <ccoff1798@gmail.com>
2023-08-09 20:57:14 -0600

    working well enough, got css kinda going now

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,38 @@
+//global variables needed
 const search = document.getElementById('search');
 const apiKey = '853c7e45dec1706d5f746a46f8d87cf8';
 let cityName = '';
 let currentWeatherContainer = document.getElementById('current-weather-container');
 
+//intializes everything
 function searchHandler() {
     let userInput = document.querySelector('input');
     cityName = userInput.value;
-    getCoords(cityName);
     searchHistory(cityName);
+    getCoords(cityName);
 }
 
+//Pulls search history if applicable
 function searchHistory(cityName) {
     let recentSearch = cityName;
-    localStorage.setItem('history', recentSearch);
-    let pEl = document.createElement('p');
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+    history.push(recentSearch);
+    localStorage.setItem('history', JSON.stringify(history));
+    updateHistoryUI(history);
+}
+
+//puts history on page
+function updateHistoryUI(history) {
     let historyContainer = document.querySelector('#history-container');
-    pEl.textContent = recentSearch;
-    pEl.setAttribute('id', cityName);
-    historyContainer.append(pEl);
-    console.log(recentSearch);
+    historyContainer.innerHTML = ''; 
+    
+    //giving each unique ids
+    history.forEach((search, index) => {
+        let pEl = document.createElement('p');
+        pEl.textContent = search;
+        pEl.setAttribute('id', `search-${index}`);
+        historyContainer.appendChild(pEl);
+    });
 }
 
 function getCoords(cityName) {
@@ -41,11 +55,11 @@ function getWeather(lat, lon) {
     })
     .then(function(data) {
         console.log(data);
-        populatePage(data);
+        populateWeather(data);
     });
 }
 
-function populatePage(data) {
+function populateWeather(data) {
     let currentTemp = data.main.temp;
     let humidity = data.main.humidity;
     let feelsLike = data.main.feels_like;
@@ -74,6 +88,8 @@ function populatePage(data) {
 
     liEl4.textContent = `Description: ${description}`;
     ulEl.appendChild(liEl4);
+
+    ulEl.setAttribute("id", cityName )
     
     currentWeatherContainer.appendChild(ulEl);
 
@@ -84,32 +100,71 @@ function populatePage(data) {
 }
 
 function getForecast(lat, lon) {
-    let urlByLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+    let urlByLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     
     fetch(urlByLatLon)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        let ulEl = document.createElement('ul');
         let forecastContainer = document.getElementById('forecast-container');
-        let dayTracker = 1
+        forecastContainer.innerHTML = ''; 
         
-        for(let i = 0; i < data.list.length; i += 8){
-            let liEl = document.createElement('li');
-            if (dayTracker > 1){
-            liEl.textContent = `Tempurature ${dayTracker} days from now :${data.list[i].main.temp}`;  
-            }
-            else{
-                liEl.textContent = `Tempurature ${dayTracker} day from now :${data.list[i].main.temp}`; 
+        for(let i = 0; i < data.list.length; i += 8) {
+            let forecastItem = data.list[i];
+            let forecastDateTime = new Date(forecastItem.dt * 1000);
+            
+            const forecastItemContainer = document.createElement('div');
+            forecastItemContainer.classList.add('forecast-item');
+            
+            const pDateTime = document.createElement('p');
+            pDateTime.textContent = forecastDateTime.toLocaleString(); 
+            forecastItemContainer.appendChild(pDateTime);
+            
+            const ulEl = document.createElement('ul');
+            
+            const liEl1 = document.createElement('li');
+            liEl1.textContent = `Temperature: ${forecastItem.main.temp}`;
+            ulEl.appendChild(liEl1);
 
-            }
-            ulEl.appendChild(liEl);
-            dayTracker ++
+            const liEl2 = document.createElement('li');
+            liEl2.textContent = `Humidity: ${forecastItem.main.humidity}`;
+            ulEl.appendChild(liEl2);
+
+            const liEl3 = document.createElement('li');
+            liEl3.textContent = `Feels Like: ${forecastItem.main.feels_like}`;
+            ulEl.appendChild(liEl3);
+
+            const liEl4 = document.createElement('li');
+            liEl4.textContent = `Wind Speed: ${forecastItem.wind.speed}`;
+            ulEl.appendChild(liEl4);
+
+            const liEl5 = document.createElement('li');
+            liEl5.textContent = `Description: ${forecastItem.weather[0].description}`;
+            ulEl.appendChild(liEl5);
+
+            ulEl.setAttribute("id", cityName )
+
+            forecastItemContainer.appendChild(ulEl);
+
+            const iconUrl = `https://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png`;
+            const imgEl = document.createElement('img');
+            imgEl.setAttribute("src", iconUrl);
+            forecastItemContainer.appendChild(imgEl);
+
+            forecastContainer.appendChild(forecastItemContainer);
         }
-        forecastContainer.appendChild(ulEl);
     });
 }
 
+// called at startup
+function initialize() {
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+    updateHistoryUI(history);
+}
+
+initialize();
+
 search.addEventListener('click', searchHandler);
+
 
